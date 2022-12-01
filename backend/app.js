766d6bd585b8176cba549a51d13cb7115100a813
DS Rathore <dsrathorebsw1234@gmail.com>
2022-12-01 12:44:45 +0530

    Backend

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+dotenv.config({ path: "config.env" });
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello world</h1>")
+});
+
+app.listen(PORT, () => {
+    console.log(`Your website is running on http://localhost:${PORT}`)
+});
