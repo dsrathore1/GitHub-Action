2233b9b0624623a3d6f9223799aad1264f17778a
DS <dsrathorebsw1234@gmail.com>
2024-01-20 14:48:23 +0530

    Update main.go file

--- golang/main.go
+++ golang/main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("\n🎊 Welcome to Golang 🎊")
+
+	var fruitList = []string{"Apple", "Mango", "Orange", "Guava"}
+
+	fmt.Println("\nYour fruit list is :- ", fruitList)
+
+	fmt.Print("\nYour dynamic array's element :- ")
+
+	count := 5
+	for i := 0; i < count; i++ {
+		fmt.Print(2*i, " ")
+	}
+
 }
