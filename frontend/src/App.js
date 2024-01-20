2bfb21f8f6af70704e76fcd3da65151f8ae251bb
DS <dsrathorebsw1234@gmail.com>
2024-01-20 15:09:39 +0530

    Initialize frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="bg-[#1e1e1e] flex justify-center h-screen w-full items-center text-white text-3xl">Hello World</h1>
+    </>
+  );
+}
